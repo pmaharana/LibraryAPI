84beb040a177726ee6b809b1575083ec5b6b085c
Pranay Maharana <pranay.maharana@gmail.com>
2017-04-09 11:47:33 -0400

    --trying out insert

---
+++ LibraryAPI/Controllers/LibraryController.cs
@@ -0,0 +1,67 @@
+﻿using LibraryAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LibraryAPI.Controllers
+{   
+
+    public class LibraryController : ApiController
+    {
+        //the connection string to my SQL table
+        const string connectionString =
+                @"Server=localhost\SQLEXPRESS;Database=LibraryAPI;Trusted_Connection=True;";
+
+        
+
+
+
+        [HttpGet]
+        public IEnumerable<Library> GetLibraryInfo()
+        {
+            return Library.GetAllBooks(connectionString);
+        }
+
+        [HttpGet]
+        public IHttpActionResult GetBook(int id)
+        {
+            var book = Library.GetAllBooks(connectionString).FirstOrDefault(f => f.Id == id);
+            return Ok(book);
+        }
+
+        [HttpPut]
+        public IHttpActionResult CreateBook([FromBody]Library libraryBook)
+        {
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var text = @"insert into [Catalog] (Title, Author, YearPublished) 
+                    values  (@Title, @Author, @YearPublished)";
+
+                var cmd = new SqlCommand(text, connection);
+                
+                cmd.Parameters.AddWithValue("@Title", libraryBook.Title);
+                cmd.Parameters.AddWithValue("@Author", libraryBook.Author);
+                cmd.Parameters.AddWithValue("@YearPublished", libraryBook.YearPublished);
+                connection.Open();
+                cmd.ExecuteNonQuery();
+                connection.Close();
+
+            }
+
+            return Ok(libraryBook);
+        }
+
+
+
+
+
+
+
+
+    }
+
+}
--- LibraryAPI/Models/Library.cs
+++ LibraryAPI/Models/Library.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,13 +8,59 @@ namespace LibraryAPI.Models
 {
     public class Library
     {
+        private SqlDataReader reader;
+
+        public int Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public int YearPublished { get; set; }
         public string Genre { get; set; }
-        public bool IsCheckedOut { get; set; }
-        public DateTime LastCheckedOutDate { get; set; }
-        public DateTime DueBackDate { get; set; }
+        public string IsCheckedOut { get; set; }
+        public string LastCheckedOutDate { get; set; }
+        //public DateTime DueBackDate { get; set; }
 
+        public Library()  //base constructor
+        {
+
+        }
+
+        public Library(SqlDataReader reader)  //adding to Library constructor
+        {
+            Id = (int)reader[0];
+            Title = reader[1].ToString();
+            Author = reader[2].ToString();
+            YearPublished = (int)reader[3];
+            Genre = reader[4].ToString();
+            IsCheckedOut = reader[5].ToString();
+            LastCheckedOutDate = reader[6].ToString();
+
+        }
+
+
+        public static List<Library> GetAllBooks(string connectionString)
+        {
+            
+
+            var books = new List<Library>();  //created a new list to store each book added
+            using (var connection = new SqlConnection(connectionString))
+            {
+                var text = @"select Id, Title, Author, YearPublished, Genre, IsCheckedOut, LastCheckedOutDate
+            from Catalog";
+                var sqlCommand = new SqlCommand(text, connection);
+                connection.Open();
+                var reader = sqlCommand.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    var book = new Library(reader);
+                    books.Add(book);
+                }
+                connection.Close();
+                return books;
+
+            }
+        }
     }
+
+    
 }
